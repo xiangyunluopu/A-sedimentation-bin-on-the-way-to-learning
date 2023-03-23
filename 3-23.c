f5c4a1aa99537300a3cb69668a7e8f9d953acb0d
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-03-23 23:25:06 +0800

    Add files via upload

---
+++ 3-23.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<Windows.h>
+//int welcome()
+//{
+//	puts("欢迎来到我制作的临时菜单！！\n\n按下1，打印你好！！\n按下0，退出程序！！\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int j = 0;
+//	for (;;)
+//	{
+//		welcome();
+//		scanf("%d", &j);
+//		system("cls");
+//		if (0 == j)
+//		{
+//			puts("即将退出！！");
+//			system("pause");
+//			system("cls");
+//			break;
+//		}
+//		if (1 == j);
+//		{
+//			puts("你好！！");
+//			system("pause");
+//			system("cls");
+//		}
+//		if (0 != j && 1 != j)
+//		{
+//			puts("输入错误，请重新输入！！");
+//			system("pause");
+//			system("cls");
+//		}
+//					
+//	}
+//	return 0;
+//}
+//int welcome()
+//{
+//	puts("欢迎来到我临时制作的菜单！！\n");
+//	puts("按下1，打印你好！！\n");
+//	puts("按下0，退出程序！！\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int j = 0;
+//	for (;;)
+//	{
+//		welcome();
+//		system("pause");
+//		system("cls");
+//		scanf("%d", &j);
+//		switch (j)
+//		{
+//			case 0:
+//			{
+//				puts("即将退出！！");
+//				system("pause");
+//				system("cls");
+//				break;
+//			}
+//			case 1:
+//			{
+//				puts("你好！！");
+//				system("pause");
+//				system("cls");
+//				break;
+//			}
+//			default:
+//			{
+//				puts("输入错误，请重新输入！！");
+//				system("pause");
+//				system("cls");
+//				break;
+//			}
+//		}
+//		if (0 == j)
+//		{
+//			break;
+//		}
+//	}
+//	return 0;
+//}
