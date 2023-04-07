2e4ff138e9a47b816dd913c7ac25dbd77c8b9e84
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-04-07 12:57:57 +0800

    Add files via upload

---
+++ tese-4-7.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#define A(a1, s2, s3, s4, s5, d) a1 = A + B + C + D + E; s2 = A + B + C + D + E; s3 = A + B + C + D + E; s4 = A + B + C + D + E; s5 = A + B + C + D + E; d = A + B + C + D + E;
+//
+//#include<stdio.h>
+//
+//int main()
+//{
+//
+//	int A = 1, B = 2, C = 3, D = 4, E = 5, aa = 0, sb = 0, sc = 0, sd = 0, se = 0, dd = 0;
+//
+//	A(aa, sb, sc, sd, se, dd);
+//
+//	printf("sa = %d, sb = %d, sc = %d, sd = %d, se = %d, dd = %d\n", aa, sb, sc, sd, se, dd);
+//
+//	return 0;
+//}
+
+//#define CP(A, B) (A > B ? A : B)
+//
+//#include<stdio.h>
+//#include<time.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int ret = 0;
+//	srand((unsigned int)time(NULL));
+//
+//	a = rand() % 100;
+//	b = rand() % 100;
+//	printf("a = %d  b = %d\n", a, b);
+//
+//	ret = CP(a, b);
+//
+//	printf("ret = %d\n", ret);
+//
+//	return 0;
+//}
