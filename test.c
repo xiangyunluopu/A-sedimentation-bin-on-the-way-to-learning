0beedfeab95105414374eccf2d76a50029b103ce
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-04-08 18:57:34 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,28 +1,104 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include"game.h"
+#include<stdio.h>
 
-int main()
-{
-	int t = 0;
-	srand((unsigned int)time(NULL));
+//int main()
+//{
+//	float a;
+//	a = 5 / 2.0;
+//	printf("a = %f", a);
+//	return 0;
+//}
 
-	do
-	{
-		menu();
-		scanf("%d", &t);
-		switch (t)
-		{
-		case 0:
-			break;
-		case 1:
-			play();
-			break;
-		default:
-			printf("输入错误，请重新输入\n");
-			break;
-		}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	a <<= 1;
+//	printf("%d", a);
+//	return 0;
+//}
 
-	} while (t);
+//int main()
+//{
+//	int a = 3, b = 5;
+//	printf("a = %d, b = %d\n", a, b);
+//	a = a * b;
+//	b = a / b;
+//	a = a / b;
+//	/*a = a + b;
+//	b = a - b;
+//	a = a - b;*/
+//	//int c = a & b;
+//	//// 011
+//	//// 101
+//	//// 001
+//	//printf("%d\n", c);// 1
+//	//c = a | b;
+//	//// 011
+//	//// 101
+//	//// 111
+//	//printf("%d\n", c);// 7
+//	//a = a ^ b;
+//	//b = a ^ b;
+//	//a = a ^ b;
+//	printf("a = %d, b = %d\n", a, b);
+//	return 0;
+//}
 
-	return 0;
-}
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	int a = 0;
+//	scanf("%d", &a);
+//
+//	for (; a;)
+//	{
+//		count += 1;
+//		a = a & (a - 1);
+//	}
+//
+//	/*for (i = 0; i < 32; i += 1)
+//	{
+//		if (a >> i & 1 == 1)
+//		{
+//			count += 1;
+//		}
+//	}*/
+//	/*for (i = 0; i < (sizeof(a) * 8); i += 1)
+//	{
+//		if (a & 1 == 1)
+//		{
+//			count += 1;
+//		}
+//		a >>= 1;
+//	}*/
+//	/*for (;a;)
+//	{
+//		if (a % 2 == 1)
+//		{
+//			count += 1;
+//		}
+//		a /= 2;
+//	}*/
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	char b = 0;
+//	char arr[10] = { 0 };
+//	int* p = &a;
+//	printf("%d\n", p);
+//	printf("%d\n", *p);
+//
+//
+//	/*a = 0;
+//	if (!a)
+//	{
+//		printf("hehe\n");
+//	}*/
+//	return 0;
+//}
