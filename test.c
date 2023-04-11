0b614e858514cf8a52d885ade4a5d99b99a43f76
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-04-11 23:20:04 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,79 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <stdlib.h>
 
-//int main()
-//{
-//	int a = 0;
-//	printf("%d\n", ~a);
-//	return 0;
-//}
+struct stu
+{
+	char name[20];
+	int age;
+	char id[20];
+};
 
-//int main()
-//{
-//	int a = 3;
-//	int b = 5;
-//	int c = 0;
-//	/*c = (a > b ? a + a : b + b);*/
-//	if (b > a)
-//	{
-//		c = a + a;
-//	}
-//	else
-//	{
-//		c = b + b;
-//	}
-//	printf("%d\n", c);
-//	return 0;
-//}
+struct p
+{
+	struct stu* next;
+};
 
-//int main()
-//{
-//	int a = 0;
-//	int b = 0;
-//	int c = 1000;
-//
-//	/*if (a = a + 1, b = b + a, c < b)
-//	{
-//		printf("hehe\n");
-//	}*/
-//	while (a += 1, b += a, (c -= b) > 0)
-//	{
-//		printf("hehe");
-//	}
-//	printf("\n");
-//
-//	return 0;
-//}
-
-//int CP(int x, int y)
-//{
-//
-//	return x > y ? x : y;
-//}
-//
-//int main()
-//{
-//	int a = 0;
-//	int b = 0;
-//	int c = 0;
-//	scanf("%d%d", &a, &b);
-//	c = CP(a, b);
-//	printf("%d\n", c);
-//	return 0;
-//}
-
-//struct me
-//{
-//	char name[20];
-//	char school[40];
-//	int age;
-//};
-//
-//int main()
-//{
-//	struct me I = { "周海涛", "湖南高速铁路职业技术学院", 20 };
-//	printf("名字:%s\n学校:%s\n年龄:%d\n", I.name, I.school, I.age);
-//	return 0;
-//}
-
+int main()
+{
+	struct stu s1 = { "张三", 20, "4988295" };
+	struct stu* p = &s1;
+	struct p* A = (struct p*)malloc(sizeof(struct p));
+	A->next = p;
+	printf("%s\n", (*p).name);
+	printf("%d\n", p->age);
+	printf("%s\n", A->next->id);
+	
+	/*printf("%s\n", s1.name);
+	printf("%d\n", s1.age);
+	printf("%s\n", s1.id);*/
+	return 0;
+}
