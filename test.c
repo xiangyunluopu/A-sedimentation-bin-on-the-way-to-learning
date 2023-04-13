2efc13200bff3ae854b7aae45a978c9cba0828af
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-04-13 12:42:58 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,86 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-//#include <stdio.h>
-//
-//int main(){
-//	int arr[100] = { 0 };
-//	int sum = 0;
-//	int j = 0;
-//
-//	scanf("%d %d", &sum, &j);
-//	if (0 == sum || 0 == j) {
-//		return 0;
-//	};
-//
-//	int i = 0;
-//	for (i = 0; i < sum; i += 1) {
-//		arr[i] = i + 1;
-//	}
-//	
-//	int count = 0;
-//	int ret = 0;
-//	int sub = 0;
-//	for (ret = sum, count = 1;ret > 1;) {
-//		if (arr[sub] == 0) {
-//			sub = (sub + 1) % sum;
-//		}
-//		else {
-//			if (count != j) {
-//				sub = (sub + 1) % sum;
-//				count += 1;
-//			}
-//			else {
-//				arr[sub] = 0;
-//				count = 1;
-//				ret -= 1;
-//			}
-//		}
-//	}
-//
-//	for (i = 0; i < sum; i += 1) {
-//		if (0 != arr[i]) {
-//			printf("%d", arr[i]);
-//		}
-//	}
-//
-//	return 0;
-//}
-
 #include <stdio.h>
 
 int main() {
 	int arr[100] = { 0 };
-	int p = 0;
-	int q = 0;
-	int sum = 0;
+	int pre = 0;
+	int last = 0;
+	int m = 0;
 	int n = 0;
-	scanf("%d %d", &sum, &n);
-	if (0 == sum || 0 == n) {
-		return 0;
-	}
+	scanf("%d %d", &m, &n);
 	int i = 0;
-	for (i = 0; i < sum; i += 1) {
- 		arr[i] = i + 1;
+ 	for (i = 0; i < m; i += 1) {
+		arr[i] = i + 1;
 	}
-	arr[i] = 0;
-	int count = 0;
-	p = 0;
-	q = sum - 1;
-	for (count = 1; p != q;) {
-		if (count != n) {
-			q = p;
-			p = arr[p];
+	arr[i - 1] = 0;
+	pre = 0;
+	last = m - 1;
+	int t = m;
+	int count = 1;
+	for (; t > 1;) {
+		if (count == n)
+		{
+			arr[last] = arr[pre];
+			pre = arr[last];
+			count = 1;
+			t -= 1;
+		}
+		else {
+			last = pre;
+			pre = arr[last];
 			count += 1;
 		}
-		else {  
-			// [2] [2] [3] [4] [5] [6] [7] [8] [9] [0]
-			// [0] [1] [2] [3] [4] [5] [6] [7] [8] [9]
-			//  q       p
-			arr[q] = arr[p];
-			p = arr[q];
-			count = 1;
-		}
 	}
-	printf("%d", &arr[p]);
+
+	printf("%d\n", arr[pre] + 1);
 	return 0;
 }
