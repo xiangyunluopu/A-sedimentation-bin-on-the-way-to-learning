15a891f69d7c31b273faa8c0d8b49fd38beb8b9b
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-04-18 18:02:16 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -2,66 +2,112 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-typedef struct listnode {
-	int data;
-	struct listnode* next;
-}LL;
+typedef struct list_node {
+	int a;
+	int b;
+	int c;
+	struct list_node* next;
+}LLL;
+
 
 int main() {
+	srand((unsigned int)time(NULL));   // 利用时间戳来作为随机数生成函数的起点
+	LLL* head = (LLL*)malloc(sizeof(LLL));
 	int i = 0;
 	int j = 0;
-	LL* head = (LL*)malloc(sizeof(LL));
-	LL* q = NULL;
-	LL* p = NULL;
+	int L = 0;
+	int num1 = 0;
+	int num2 = 0;
+	LLL* A = NULL;
+	LLL* B = NULL;
+	LLL* C = NULL;
+	LLL* D = NULL;
+	LLL* p = NULL;
+	LLL* s = NULL;
 	head->next = NULL;
-	/***********创建十个结构体************/
-	for (i = 1, q = head; i <= 10; i += 1) {
-		p = (LL*)malloc(sizeof(LL));
-		if (NULL == p) {
-			exit(1);
+	for (;;) {
+		j = rand() % 21;
+		if (2 < j && j <= 20) {
+			break;
 		}
-		p->data = i;
-		p->next = q->next;
-		q->next = p;
-		q = p;
 	}
-	/*===================================*/
-	int data = 0;
-	for (p = head->next, i = 0; i < 10; i += 1) {
-		data = p->data;
-		printf("%3d", data);
-		p = p->next;
+	A = head;
+	for (i = j; i > 0; i -= 1) {
+		B = (LLL*)malloc(sizeof(LLL));
+		num1 = rand() % 11;
+		num2 = rand() % 11;
+		B->a = num1;
+		B->b = num2;
+		B->c = (num1 + num2);
+		B->next = A->next;
+		A->next = B;
+		A = A->next;
 	}
-	/***********将单链表倒过来************/
-	p = head;
-	for (i = 1; i <= 10; i += 1) {
-
-		q = p->next;
-		p = p->next;
+	A = NULL;
+	B = NULL;
+	// 随机结点个数与随机值都生成完毕
+	// 打印
+	A = head->next;
+	for (L = 0; A != NULL; A = A->next) {
+		printf("%3d", A->c);
+		L += 1;
 	}
-	// 此时的q->最后一个结点；
-	p = head; 
-	LL* s = head;
-	for (j = 10; j > 0; j -= 1) {
-		for (i = 0; i < j - 1; i += 1) {
-			s = s->next;
+	A = NULL;
+//                                                    A        B         C        D
+	// [1][->1] [2][->3] [3][->4] [4][->5] [5][->6] [6][->7] [7][->8] [8][NULL]  NULL
+	for (i = 0; i < L - 1; i += 1) {
+		A = head;
+		B = head->next;
+		C = B->next;
+		D = C->next;
+		for (j = 0 + i; j < L - 1; j += 1) {
+			if (B->c > C->c) {
+				A->next = C;
+				C->next = B;
+				B->next = D;
+				B = A->next;
+				C = B->next;
+			}
+			if (j < L - 2) {
+				D = D->next;
+			}
+			C = C->next;
+			B = B->next;
+			A = A->next;
 		}
-		p = s->next;
-		p->next = s;
-		s = head;
 	}
-	p = head->next;
-	p->next = NULL;
-	head->next = q;
-	/*====================================*/
-	printf("\n\n\n");
-	data = 0;
-	for (p = head->next, i = 0; i < 10; i += 1) {
-		data = p->data;
-		printf("%3d", data);
-		p = p->next;
+	A = NULL; B = NULL; C = NULL; D = NULL;
+	printf("\n\n");
+	A = head->next;
+	for (L = 0; A != NULL; A = A->next) {
+		printf("%3d", A->c);
+		L += 1;
 	}
-
+	A = NULL;
 	return 0;
 }
+
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int arr[7] = { 2, 4, 8, 32, 0, 9, 7 };
+//	int i = 0;
+//	int j = 0;
+//	int n = 0;
+//	for (j = 7; j > 0; j -= 1) {
+//		for (i = 0; i < j - 1; i += 1) {
+//			if (arr[i] > arr[i + 1])
+//			{
+//				n = arr[i + 1];
+//				arr[i + 1] = arr[i];
+//				arr[i] = n;
+//			}
+//		}
+//	}
+//
+//
+//	return 0;
+//}
