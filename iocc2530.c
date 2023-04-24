4c4aca01315288dd34f6a795d2665ef04f9fafcb
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-04-24 16:48:02 +0800

    Add files via upload

--- iocc2530.c
+++ iocc2530.c
@@ -1,27 +1,51 @@
 #include "ioCC2530.h"
 
-#define D6 P1_4
-void Delay(unsigned int time)
-{
-  unsigned char j = 0;
-  unsigned int i = 0;
-  for (i = 0; i < time; i += 1)
-  {
-    for (j = 0; j < 240; j += 1)
-    {
-      asm("NOP");asm("NOP");asm("NOP");
-    }
-  }
+#define LED1 P1_0
+#define LED2 P1_5
+#define LED3 P1_3
+#define LED4 P1_4
+
+#define SW1 P1_2
+
+int count = 0;
+
+void init() {
+  // 0011 1001
+  P1SEL &= ~0x39;
+  P1DIR |= 0x39;
+  P1 &= ~0x39;
+  EA = 1;
+  // 0001 0000
+  IEN2 |= 0x10;
+  P1IEN |= 0x04;
+  PICTL |= 0x02;
 }
 
-void main()
-{
-  P1SEL &= ~0x10;
-  P1DIR |= 0x10;
-  D6 = 0;
-  for (;;)
-  {
-    D6 = ~D6;
-    Delay(1000);
+#pragma vector = P1INT_VECTOR
+__interrupt void A() {
+  if (P1IFG & 0x04) {
+    if (0 == LED1) {
+      count += 1;
+      if (3 == count) {
+        LED1 = 1;
+        count = 0;
+      }
+    }
+    else if (1 == LED1) {
+      count += 1;
+      if (5 == count) {
+        LED1 = 0;
+        count = 0;
+      }
+    }
+    P1IFG &= ~0x04;
+  }
+  P1IF = 0;
+}
+  
+void main() {
+  init();
+  for (;;) {
+    
   }
 }
