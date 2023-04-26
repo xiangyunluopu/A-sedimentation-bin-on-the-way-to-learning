2a88c3d301afb288b5fe7996f9737dc268326629
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-04-26 18:34:51 +0800

    Add files via upload

---
+++ test2[4-26].c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main() {
+//	int a = 10;
+//	int* pa = &a;
+//	int** ppa = &pa;
+//	printf("a = %d\n", **ppa);
+//	**ppa = 20;
+//	printf("a = %d\n", a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int* par[3] = { NULL };
+//	int i = 0;
+//	par[0] = &a;
+//	par[1] = &b;
+//	par[2] = &c;
+//	for (i = 0; i < 3; i += 1) {
+//		*par[i] = i + 1;
+//	}
+//	for (i = 0; i < 3; i += 1) {
+//		printf("%d ", *par[i]);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//	int* par[3];
+//	int arr[3] = { 2, 4, 6};
+//	int i = 0;
+//	for (i = 0; i < 3; i += 1) {
+//		par[i] = &arr[i];
+//	}
+//	for (i = 0; i < 3; i += 1) {
+//		printf("%d ", *par[i]);
+//	}
+//	return 0;
+//}
+
