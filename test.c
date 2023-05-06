5216ccf4b04013fea8d68c61a82dbabe44ab845c
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-05-06 23:15:31 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,100 +1,113 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-#define size 100
+//#include<stdio.h>
+//
+//typedef struct {
+//	int data[10];
+//	int top;
+//}Sta;
+//
+//void ana(Sta* x, int n) {
+//	if (n) {
+//		x->top += 1;
+//		x->data[x->top] = n % 10;
+//		ana(x, n / 10);
+//	}
+//}
+//
+//void print(Sta* x, int n) {
+//	int i = 0;
+//	ana(x, n);
+//	for (i = x->top; i > -1; i -= 1) {
+//		printf("%d", x->data[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int main(void) {
+//	Sta x;
+//	x.top = -1;
+//	int n = 1234;
+//	print(&x, n);
+//	return 0;
+//}
 
-#include <stdio.h>
+//#include<stdio.h>
+//
+//int main(void) {
+//	printf("%d\n", 1234 % 10);
+//	return 0;
+//}
 
-void trans(char exp[size], char possexp[size]) {
-	int j = 0;
-	struct zhang {
-		char f[size];
-		int top;
-	};
-	struct zhang op;
-	op.top = -1;
-	for ( j = -1; *exp != '\n';) {
-		switch (*exp) {
-		case '+':
-		case '-':
-			if (op.f[op.top] != '(' && op.top != -1) {
-				for (; op.f[op.top] != '(' && op.top != -1;) {
-					j += 1;
-					possexp[j] = op.f[op.top];
-					op.top -= 1;
-				}
-			}
-			op.top += 1;
-			op.f[op.top] = *exp;
-			exp++;
-			break;
-		case '*':
-		case '/':
-			if (op.f[op.top] == '*' || op.f[op.top] == '/') {
-				for (; op.top != -1 && (op.f[op.top] == '*' || op.f[op.top] == '/');) {
-					j += 1;
-					possexp[j] = op.f[op.top];
-					op.top -= 1;
-				}
-			}
-			
-			op.top += 1;
-			op.f[op.top] = *exp;
-			exp += 1;
-			break;
-		case '(':
-			op.top += 1;
-			op.f[op.top] = *exp;
-			exp += 1;
-			break;
-		case ')':
-			for (; op.f[op.top] != '(';) {
-				j += 1;
-				possexp[j] = op.f[op.top];
-				op.top -= 1;
-			}
-			exp += 1;
-		case ' ':
-			exp += 1;
-			break;
-		default:
-			for (; '0' <= *exp && *exp <= '9';) {
-				j += 1;
-				possexp[j] = *exp;
-				exp += 1;
-			}
-			j += 1;
-			possexp[j] = '#';
-			break;
-		}
-		
-	}
-	j += 1;
-	possexp[j] = '\0';
-	printf("%s", possexp);
-	
-}
+//#include<stdio.h>
+//
+//int main(void) {
+//	int n = 0;
+//	int i = 0;
+//	int num = 0;
+//	scanf("%d", &n);
+//	for (i = 1, num = 1; i <= n; i += 1) {
+//		num = i * num;
+//	}
+//	printf("%d\n", num);
+//	return 0;
+//}
 
-int main() {
-	char exp[size] = { 0 };
-	char possexp[size] = { 0 };
-	scanf("%s", exp);
+//#include<stdio.h>
+//
+//int n_n_n(int n) {
+//	if (n > 1)
+//		return n * n_n_n(n - 1);
+//	else if (n == 1)
+//		return 1;
+//}
+//
+//int main(void) {
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", n_n_n(n));
+//	return 0;
+//}
 
-	trans(exp, possexp);
+//#include<stdio.h>
+//
+//int my_strlen(char* str) {
+//	int count = 0;
+//	while (*str != '\0') {
+//		str += 1;
+//		count += 1;
+//	}
+//	return count;
+//}
+//
+//void reverse_string(char* str) {
+//	int sz = my_strlen(str);
+//	//printf("%d", sz);
+//	int lift = 0;
+//	int right = sz - 1;
+//	char tmp = 0;
+//	while (lift < right) {
+//		tmp = *(str + lift);
+//		*(str + lift) = *(str + right);
+//		*(str + right) = tmp;
+//		/*tmp = str[lift];
+//		str[lift] = str[right];
+//		str[right] = tmp;*/
+//		lift += 1;
+//		right -= 1;
+//	}
+//	printf("%s\n", str);
+//}
+//
+//int main(void) {
+//	char str[] = "abcdefg";
+//	reverse_string(str);
+//	
+//	return 0;
+//}
+#include<stdio.h>
+
+int main(void) {
 
 	return 0;
 }
-
-//#include <stdio.h>
-//
-//int i = 0;
-//
-//int main() {
-//
-//	printf("[%d]hello,world!\n", i);
-//
-//	
-//		main();
-//	
-//
-//	return 0;
-//}
