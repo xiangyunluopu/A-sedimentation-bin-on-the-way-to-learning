456caf84ba66fdec7d4a5be6d7cf0ba17ba929ee
xiangyunluopu <128067047+xiangyunluopu@users.noreply.github.com>
2023-05-10 23:30:06 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,128 +1,103 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//
-//#include<stdio.h>
-//
-////void main(void) {
-////
-////	
-////}
-//
-//int main(void) {
-//
-//	//if (!0) { // 0 表示假，！0真
-//	//	printf("hello,world!\n");
-//	//}
-//
-//	int n = 0;
-//	scanf("%d", &n); // &取地址符号
-//	/*8 % 2 = 0;
-//	9 % 2 = 1;*/
-//	if (n % 2) {
-//		printf("奇数\n");
-//	}
-//	else {
-//		printf("偶数\n");
-//	}
-//	
-//
-//	return 0;
-//}
-
-//#include <stdio.h>
-//
-//int main(void) {
-//	int i = 0;
-//	// i = i + 1;
-//	// i += 1;
-//	// i++  先使用后加一
-//	// ++i	先加一后使用
-//	/*for (i = 1; i <= 100; i += 1) {
-//		if (i % 2) {
-//			printf("%d ", i);
-//		}
-//	}*/
-//	int n = 0;
-//	scanf("%d", &n);
-//
-//	if (n < 18) {
-//		printf("未成年\n");
-//	}
-//	else if (n >= 18 && n < 29) {
-//		printf("青年\n");
-//	}
-//	else {
-//		printf("这个人快很大了\n");
-//	}
-//
-//	return 0;
-//}
-
-//#include<stdio.h>
-//
-//int main(void) {
-//	int n = 0;
-//	scanf("%d", &n);
-//	switch (n) {
-//	case 1:
-//		printf("星期1\n");
-//		break;
-//	case 2:
-//		printf("星期2\n");
-//		break;
-//	case 3:
-//		printf("星期3\n");
-//		break;
-//	case 4:
-//		printf("星期4\n");
-//		break;
-//	case 5:
-//		printf("星期5\n");
-//		break;
-//	case 6:
-//		printf("星期6\n");
-//		break;
-//	case 7:
-//		printf("星期7\n");
-//		break;
-//	default: 
-//		printf("输入不合法\n");
-//	}
-//	return 0;
-//}
 
 #include<stdio.h>
 
-int main(void) {
-	int day = 0;
-	scanf("%d", &day);
-	/*if (day >= 1 && day <= 5) {
-		printf("weekday\n");
-	}
-	else {
-		if (day >= 6 && day <= 7) {
-			printf("weekend\n");
-		}
-		else {
-			
-			printf("输入不合法\n");
-		}
-	}*/
+#define MAXSIZE 100
+#define DataType int
 
-	/*if () {
+typedef struct Linklistnode{
+	DataType data;
+	LinkListQ* next;
+}LinkListQ;
 
-	}
-	else if () {
+typedef struct Queue {
+	LinkListQ* front;
+	LinkListQ* rear;
+}LinkQueue;
 
-	}
-	else if () {
+//void EmptyQueue(LinkQueue* Q) {
+//	if (Q->front == Q->rear) {
+//		return 1;
+//	}
+//	else {
+//		return 0;
+//	}
+//}
 
-	}
-	else {
+//int DeQueue(LinkQueue* Q, DataType* x) {
+//	LinkListQ* p = NULL;
+//	
+//	if (Q->front == Q->rear) {
+//		printf("队空，不能出队！\n");
+//		return 0;
+//	}
+//	else {
+//		p = Q->front->next;
+//		*x = p->data; // x = Q->front->next->data;
+//		Q->front->next = p->next;
+//		if (NULL == p->next)
+//			Q->rear = Q->front;
+//		free(p);
+//		return 1;
+//	}
+//}
 
-	}*/
+//int DeQueue(LinkQueue* Q, DataType* x) {
+//	LinkListQ* p = NULL;
+//	if (Q->front == Q->rear) {
+//		printf("队空，不能出队元素！\n");
+//		return 0;
+//	}
+//	else {
+//		p = Q->front->next;
+//		*x = p->data;
+//		Q->front->next = p->next;
+//		if (NULL == p->next)
+//			Q->rear == Q->front;
+//		free(p);
+//		return 1;
+//	}
+//}
 
+//int DeQueue(LinkQueue* Q, DataType* x) {
+//	LinkListQ* p = NULL;
+//	if (Q->front == Q->rear) {
+//		return 0;
+//	}
+//	else {
+//		p = Q->front->next; // 将队列中的head结点的地址赋值给p
+//		*x = p->data; // 将队首的data赋值给x指针所指向的地址的内存空间中
+//		Q->front->next = p->next; // 将队列的head结点中的front指针指向队首后面的一个结点
+//		if (NULL == p->next) {
+//			Q->rear = Q->front; // 将rear指针也指向front指着所指向的head结点
+//		}
+//		free(p); // 释放p所指向的结点
+//		return 1;
+//	}
+//}
 
+//int GetFront(LinkQueue* Q, DataType* x) {
+//	if (Q->front == Q->rear) {
+//		printf("队空，无队首元素！\n");
+//		return 0;
+//	}
+//	else {
+//		*x = Q->front->next->data; // 取队首元素
+//		return 1;
+//	}
+//}
 
-
-	return 0;
-}
+//typedef struct {
+//	DataType data[MAXSIZE];
+//	int top;
+//}Queue;
+//
+//int InQueue(LinkQueue* Q, DataType x) {
+//	if ((Q->rear + 1) % MAXSIZE == Q->front) { // 判断队是否满
+//		printf("队满，不能入队！\n");
+//		return 0;
+//	}
+//	else {
+//
+//	}
+//}
